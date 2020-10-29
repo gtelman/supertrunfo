cdd052e1ba89c74ee7cc567c2a243282a56e9d1a
Gabrielle Telman <gabrielletelman@gmail.com>
2020-10-28 22:09:49 -0300

    refactoring and adding oo

---
+++ app/src/main/java/com/wcc/supertrunfo/Player.kt
@@ -0,0 +1,7 @@
+package com.wcc.supertrunfo
+
+class Player (
+        var name: String,
+        var isWinner: Boolean = false
+
+        )
---
+++ app/src/main/java/com/wcc/supertrunfo/Vehicle.kt
@@ -0,0 +1,12 @@
+package com.wcc.supertrunfo
+
+class Vehicle (
+        var maxAcceleration: Int,
+        var accelerationTime: Int,
+        var passengers: Int,
+        var weight: Int,
+        var doors: Int,
+        var style: String,
+        var gears: Int,
+        var type: String
+)
